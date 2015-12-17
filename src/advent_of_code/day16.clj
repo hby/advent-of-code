46eb77f2c0a4c3b4b804ca26d5d7a8b70ef7190e
Bret Young <hby@acm.org>
2015-12-17 08:21:14 -0600

    Day 16

---
+++ src/advent_of_code/day16.clj
@@ -0,0 +1,40 @@
+(ns advent-of-code.day16
+  (:require [clojure.string :as s]))
+
+(def puzzle-text (slurp "resources/input/day16.txt"))
+
+(def puzzle
+  (map read-string (s/split-lines puzzle-text)))
+
+(defn compare-if-key
+  [m v c k]
+  (if (contains? m k)
+    (c v (m k))
+    true))
+
+(comment
+  (->> puzzle
+       (filter #(compare-if-key % 3 = :children))
+       (filter #(compare-if-key % 7 = :cats))
+       (filter #(compare-if-key % 2 = :samoyeds))
+       (filter #(compare-if-key % 3 = :pomeranians))
+       (filter #(compare-if-key % 0 = :akitas))
+       (filter #(compare-if-key % 0 = :vizslas))
+       (filter #(compare-if-key % 5 = :goldfish))
+       (filter #(compare-if-key % 3 = :trees))
+       (filter #(compare-if-key % 2 = :cars))
+       (filter #(compare-if-key % 1 = :perfumes)))
+  ;=> ({:sue 213, :children 3, :goldfish 5, :vizslas 0})
+  (->> puzzle
+       (filter #(compare-if-key % 3 = :children))
+       (filter #(compare-if-key % 7 < :cats))
+       (filter #(compare-if-key % 2 = :samoyeds))
+       (filter #(compare-if-key % 3 > :pomeranians))
+       (filter #(compare-if-key % 0 = :akitas))
+       (filter #(compare-if-key % 0 = :vizslas))
+       (filter #(compare-if-key % 5 > :goldfish))
+       (filter #(compare-if-key % 3 < :trees))
+       (filter #(compare-if-key % 2 = :cars))
+       (filter #(compare-if-key % 1 = :perfumes)))
+  ;=> ({:sue 323, :perfumes 1, :trees 6, :goldfish 0})
+  )
