af8e85f3672c7b094b955bab42dabaa0a1f7b8bc
Bret Young <hby@acm.org>
2017-12-09 19:27:47 -0600

    2017 day 4, part 2

--- src/advent_of_code/2017/day04.clj
+++ src/advent_of_code/2017/day04.clj
@@ -1,5 +1,6 @@
 (ns advent-of-code.2017.day04
-  (:require [clojure.string :as cs]))
+  (:require [clojure.string :as cs]
+            [clojure.math.combinatorics :as comb]))
 
 (def puzzle
   (->> (slurp "resources/2017/day04.txt")
@@ -21,4 +22,28 @@
 (comment
   (one puzzle)
   ; => 386
+  )
+
+(defn anagrams?
+  [w1 w2]
+  (= (sort w1) (sort w2)))
+
+(defn pairs
+  [pp]
+  (comb/combinations pp 2))
+
+(defn valid2?
+  [pp]
+  (not (some #(apply anagrams? %) (pairs pp))))
+
+(defn two
+  [puzzle]
+  (->> puzzle
+       (map valid2?)
+       (filter identity)
+       (count)))
+
+(comment
+  (two puzzle)
+  ; => 208
   )
