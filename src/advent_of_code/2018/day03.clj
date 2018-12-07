015cf4249ef7032bfb2c31f2dc236ccab6543d85
Bret Young <hby@acm.org>
2018-12-06 20:45:27 -0600

    2018 day 3, part 2

--- src/advent_of_code/2018/day03.clj
+++ src/advent_of_code/2018/day03.clj
@@ -30,3 +30,38 @@
 (comment
   (part1 input-data))
   ;; 110827)
+
+(defn overlap?
+  [[n1 x1 y1 w1 h1] [n2 x2 y2 w2 h2]]
+  (not
+    (or
+      (> x1 (dec (+ x2 w2)))
+      (> x2 (dec (+ x1 w1)))
+      (> y1 (dec (+ y2 h2)))
+      (> y2 (dec (+ y1 h1))))))
+
+(defn connected?
+  [ss e]
+  (some (partial overlap? e) ss))
+
+(defn grow-connected-sets
+  [cs e]
+  (let [c (filter #(connected? % e) cs)
+        nc (remove #(connected? % e) cs)]
+    (conj nc (apply concat (conj c [e])))))
+
+(defn connected-sets
+  [input]
+  (reduce (fn [cs e]
+            (grow-connected-sets cs e))
+          []
+          input))
+
+(defn part2
+  [input]
+  (->> (connected-sets input)
+       (filter #(= 1 (count %)))))
+
+(comment
+  (part2 input-data))
+  ;; (((116 864 693 21 17))) )
