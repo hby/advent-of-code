ffd5f122295e8de20a636d2727b8898e6a2ad413
Bret Young <hby@acm.org>
2020-12-10 20:34:22 -0600

    2020 day 10, part 2

--- src/advent_of_code/2020/day10.clj
+++ src/advent_of_code/2020/day10.clj
@@ -20,11 +20,27 @@
   ;; 1917
   #_[])
 
-(defn part2
-  [input])
+(def arrangements-m
+  (memoize
+    (fn [f r l]
+      (if (empty? r)
+        (if (= f l) 1 0)
+        (let [[r1 r2 r3] (take-while #(<= % (+ 3 f)) r)]
+          (apply +
+                 [(if r1 (arrangements-m r1 (drop 1 r) l) 0)
+                  (if r2 (arrangements-m r2 (drop 2 r) l) 0)
+                  (if r3 (arrangements-m r3 (drop 3 r) l) 0)]))))))
 
+(defn arrangements
+  [input]
+  (let [si (sort input)]
+    (arrangements-m 0 si (last si))))
+
+(defn part2
+  [input]
+  (arrangements input))
 
 (comment
   (part2 input)
-  ;; TODO
+  ;; 113387824750592
   #_[])
